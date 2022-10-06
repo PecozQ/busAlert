1b0fe42637384cc4ef56182d8fb0518f9accbd4f
PecozQ <kaviyarasan2210@gmail.com>
2022-10-06 18:26:47 +0530

    Fix: postgres DB intgr

--- config/env/development.js
+++ config/env/development.js
@@ -1,18 +1,22 @@
 module.exports = {
   datastores: {
-    default: {
+    // default: { // To run in local DB
+    //   adapter: 'sails-postgresql',
+    //   url: 'postgres://postgres:admin@localhost:5432/postgres',
+    //   connectTimeout: 300000,
+    //   // ssl: true,
+    //   database: 'postgres',
+    // },
+    default: { // To run in production DB
       adapter: 'sails-postgresql',
-      url: 'postgres://postgres:admin@localhost:5432/postgres',
+      url: 'postgresql://doadmin:do6nfsjbla4vfc7q@primary-db-postgresql-do-user-2545403-0.db.ondigitalocean.com:25060/development',
       connectTimeout: 300000,
-      // ssl: true,
+      ssl:  {
+          sslmode: 'require',
+          rejectUnauthorized: false,
+        },
       database: 'postgres',
     },
-    // snapshot: {   // To connect to second DB
-    //   adapter: 'sails-mysql',
-    //   url: 'mysql://root:password@127.0.0.1:3306/snapshot',
-    //   connectTimeout: 100000,
-    //   database: 'snapshot',
-    // }
   },
   models: {
     migrate: 'safe',
