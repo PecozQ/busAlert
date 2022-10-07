ceabcfcbeb107cc215602ee615cb46d28e6b2ef5
PecozQ <kaviyarasan2210@gmail.com>
2022-10-07 13:43:33 +0530

    Fix: gpstracker converted to array data

--- api/controllers/GpstrackerController.js
+++ api/controllers/GpstrackerController.js
@@ -5,26 +5,47 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
+function gpsDataUpdate(data, res)  {
+  return new Promise((resolve, reject) => {
+    async.each(data, async (dataItr) => {
+        var updatingObject = {
+          latitude: dataItr.latitude,
+          longitude: dataItr.longitude
+        };
+        try {
+          var deviceDetail = await Gpstracker.findOne({ deviceId: dataItr.deviceId });
+          if (!deviceDetail) { // If the device details is not present
+            updatingObject.deviceId = dataItr.deviceId;
+            var gpsCreate =  await Gpstracker.create(updatingObject).fetch();
+            // updatedData.push(...gpsCreate);
+            // return res.status(200).json({ message: 'Created successfully', gpsCreate });
+          } else {
+            var updatedDeviceDetail = await Gpstracker.updateOne({ deviceId: dataItr.deviceId }).set(updatingObject);
+            // updatedData.push(...updatedDeviceDetail);
+            // return res.status(200).json({ message: 'Updated successfully', updatedDeviceDetail });
+          }
+        } catch(err) {
+          reject(err);
+        }
+      }, (err) => {
+        if (err) { 
+          reject(err);
+         }
+         resolve('success');
+        // cb(err, activity);
+      });
+    });
+}
 module.exports = {
   gpsTracker: async function(req, res) {
-    var updatingObject = {
-      latitude: req.body.latitude,
-      longitude: req.body.longitude
-    };
-    try {
-      var deviceDetail = await Gpstracker.findOne({ deviceId: req.body.deviceId });
-      if (!deviceDetail) { // If the device details is not present
-        updatingObject.deviceId = req.body.deviceId;
-        var gpsCreate =  await Gpstracker.create(updatingObject).fetch();
-        return res.status(200).json({ message: 'Created successfully', gpsCreate });
-      } else {
-        var updatedDeviceDetail = await Gpstracker.updateOne({ deviceId: req.body.deviceId }).set(updatingObject);
-        return res.status(200).json({ message: 'Updated successfully', updatedDeviceDetail });
-      }
-    } catch(err) {
-      return res.badRequest({ error: 'gpsTracker for device not created', err });
+    if (req.body.data.length === 0) {
+      return res.badRequest({ error: 'No data is present to update' });
     }
+    gpsDataUpdate(req.body.data).then((resp) => {
+      return res.status(200).json({message: 'Created successfully'});
+    }).catch((err) => {
+      return res.badRequest(err);
+    });
   }
 };
 
